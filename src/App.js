a9eca0ead8b88097acddb7b76b3778f95ad2e602
mjbiener <michaelbiener@yahoo.com>
2021-09-01 11:59:18 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './App.css'
+import ColorChangeButton from './ColorChangeButton'
+
+function App() {
+  const [color, setColor] = useState('')
+  return (
+    <div className={`react-root ${color}`}>
+      <div className="centered">
+        <h1>Color Picker</h1>
+        <ColorChangeButton color="red" setColor={setColor} />
+        <ColorChangeButton color="blue" setColor={setColor} />
+        <ColorChangeButton color="yellow" setColor={setColor} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
